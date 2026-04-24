82bf13f886798b07ba062ba9525fdede2354b7a1
ibiscum <ulf.luecking@gmail.com>
2026-04-24 23:53:58 +0200

    test

--- chapter2/recipe2-3/footballresource/src/test/java/com/packt/footballresource/FootballControllerTest.java
+++ chapter2/recipe2-3/footballresource/src/test/java/com/packt/footballresource/FootballControllerTest.java
@@ -2,7 +2,13 @@ package com.packt.footballresource;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,19 +27,20 @@ class FootballControllerTest {
     void testGetTeams() throws Exception {
         mockMvc.perform(get("/football/teams"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.length()").value(31))
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(content().string(org.hamcrest.Matchers.containsString("Argentina")));
     }
 
-    // @Test
-    // @WithMockUser(authorities = "SCOPE_football:admin")
-    // void testAddTeam() throws Exception {
-    //     String teamName = "TestTeam";
-    //     mockMvc.perform(post("/football/teams")
-    //             .contentType("application/json")
-    //             .content("\"" + teamName + "\""))
-    //             .andExpect(status().isOk())
-    //             .andExpect(content().string(teamName + " added"));
-    // }
+    @Test
+    @WithMockUser(authorities = "SCOPE_football:admin")
+    void testAddTeam() throws Exception {
+        String teamName = "TestTeam";
+
+        mockMvc.perform(post("/football/teams")
+                        .with(csrf())
+                        .contentType(TEXT_PLAIN)
+                        .content(teamName))
+                .andExpect(status().isOk())
+                .andExpect(content().string(teamName + " added"));
+    }
 }
--- chapter3/recipe3-8/footballobs/src/test/java/com/packt/footballobs/FootballobsApplicationTests.java
+++ chapter3/recipe3-8/footballobs/src/test/java/com/packt/footballobs/FootballobsApplicationTests.java
@@ -3,7 +3,12 @@ package com.packt.footballobs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=sa"
+})
 class FootballobsApplicationTests {
 
 	@Test
