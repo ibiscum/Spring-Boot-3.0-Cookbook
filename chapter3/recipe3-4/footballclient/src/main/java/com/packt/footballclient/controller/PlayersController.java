9dda98d3d42e99444e5072e746b25b692cb2603f
ibiscum <ulf.luecking@gmail.com>
2026-05-09 06:11:53 +0200

    corrected tracing

--- chapter3/recipe3-4/footballclient/src/main/java/com/packt/footballclient/controller/PlayersController.java
+++ chapter3/recipe3-4/footballclient/src/main/java/com/packt/footballclient/controller/PlayersController.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
