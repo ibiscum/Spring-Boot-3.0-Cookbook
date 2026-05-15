aec1c318b32f4ce53c6f655153eb2ba848315232
ibiscum <ulf.luecking@gmail.com>
2026-05-15 18:11:26 +0200

    update swagger ui

--- chapter8/recipe8-1/README.md
+++ chapter8/recipe8-1/README.md
@@ -1,6 +1,6 @@
 # Spring Boot 3.0 Cookbook - Chapter 8 - recipe8-1
 
-    ./mvnw -N wrapper:wrapper -Dmaven=3.9.12
+    ./mvnw -N wrapper:wrapper -Dmaven=3.9.15
     ./mvnw spring-boot:run
 
     curl http://localhost:8080/cards
--- chapter8/recipe8-1/cards-functional/src/test/java/com/packt/cardsfunctional/CardsRouterTests.java
+++ chapter8/recipe8-1/cards-functional/src/test/java/com/packt/cardsfunctional/CardsRouterTests.java
@@ -2,7 +2,8 @@ package com.packt.cardsfunctional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+// import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.webflux.test.autoconfigure.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @WebFluxTest(CardsRouterConfig.class)
