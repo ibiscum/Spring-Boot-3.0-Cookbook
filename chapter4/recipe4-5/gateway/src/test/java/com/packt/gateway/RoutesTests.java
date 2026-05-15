002dd937c975b64e7f290c1e4aa9feffc531f844
ibiscum <ulf.luecking@gmail.com>
2026-05-15 16:04:37 +0200

    4.0.6

--- chapter4/recipe4-5/gateway/src/test/java/com/packt/gateway/RoutesTests.java
+++ chapter4/recipe4-5/gateway/src/test/java/com/packt/gateway/RoutesTests.java
@@ -8,15 +8,15 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@AutoConfigureWireMock(port = 0)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
         "PLAYERS_URI=http://localhost:${wiremock.server.port}",
         "ALBUMS_URI=http://localhost:${wiremock.server.port}",
 })
-public class RoutesTester {
+@EnableWireMock
+public class RoutesTests {
 
     @Autowired
     private WebTestClient webClient;
