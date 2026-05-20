160a72c7763b4ba50c8cc9d8e9c20c5ffc66966b
ibiscum <ulf.luecking@gmail.com>
2026-05-20 21:22:56 +0200

    7-2

--- chapter7/recipe7-2/football/src/test/java/com/packt/football/service/AlbumsServiceTest.java
+++ chapter7/recipe7-2/football/src/test/java/com/packt/football/service/AlbumsServiceTest.java
@@ -1,19 +1,14 @@
 package com.packt.football.service;
 
+import com.packt.football.config.FootballContainersConfig;
 import com.packt.football.domain.Album;
 import com.packt.football.domain.Card;
 import com.packt.football.domain.TradingUser;
 import com.packt.football.domain.User;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,34 +20,13 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.jupiter.api.Assertions.*;
 
-@Testcontainers
 @SpringBootTest
-@ContextConfiguration(initializers = AlbumsServiceTest.Initializer.class)
+@Import(FootballContainersConfig.class)
 class AlbumsServiceTest {
-
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
-            .withDatabaseName("football")
-            .withUsername("football")
-            .withPassword("football");
-
-    static class Initializer
-            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                            "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                            "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                            "spring.datasource.password=" + postgreSQLContainer.getPassword())
-                    .applyTo(configurableApplicationContext.getEnvironment());
-        }
-    }
-
-    @BeforeAll
-    public static void startContainer() {
-        postgreSQLContainer.start();
-    }
 
     @Autowired
     AlbumsService albumsService;
+
     @Autowired
     UsersService usersService;
 
@@ -65,7 +39,6 @@ class AlbumsServiceTest {
 
         assertEquals("Sample album", album.title());
         assertEquals(user.id(), album.ownerId());
-
     }
 
     @Test
@@ -91,8 +64,8 @@ class AlbumsServiceTest {
     public void useAllCardAvailable() {
         // ARRANGE
         User user = usersService.createUser("Test");
-        Album album1 = albumsService.buyAlbum(user.id(), "sample album");
-        List<Card> cards = albumsService.buyCards(user.id(), 2);
+        albumsService.buyAlbum(user.id(), "sample album");
+        albumsService.buyCards(user.id(), 2);
 
         // ACT
         List<Card> usedCards = albumsService.useAllCardAvailable(user.id());
@@ -101,6 +74,7 @@ class AlbumsServiceTest {
             assertThat(card.albumId(), notNullValue());
         }
     }
+
     @Test
     void transferCard() {
         User user1 = usersService.createUser("Test");
@@ -117,7 +91,7 @@ class AlbumsServiceTest {
 
     @Test
     void transferCard_cardNotExists() {
-        User user1 = usersService.createUser("Test");
+        //User user1 = usersService.createUser("Test");
         User user2 = usersService.createUser("Test2");
 
         Optional<Card> transferredCard = albumsService.transferCard(new Random().nextInt(), user2.id());
@@ -140,10 +114,10 @@ class AlbumsServiceTest {
         // ARRANGE
         User user1 = usersService.createUser("Test");
         User user2 = usersService.createUser("Test2");
-        Album album1 = albumsService.buyAlbum(user1.id(), "sample album");
-        Album album2 = albumsService.buyAlbum(user2.id(), "sample album");
-        List<Card> cards1 = albumsService.buyCards(user1.id(), 800);
-        List<Card> cards2 = albumsService.buyCards(user2.id(), 800);
+        albumsService.buyAlbum(user1.id(), "sample album");
+        albumsService.buyAlbum(user2.id(), "sample album");
+        albumsService.buyCards(user1.id(), 800);
+        albumsService.buyCards(user2.id(), 800);
         albumsService.useAllCardAvailable(user1.id());
         albumsService.useAllCardAvailable(user2.id());
 
@@ -156,7 +130,7 @@ class AlbumsServiceTest {
         User user1 = usersService.createUser("Test");
         Album album1 = albumsService.buyAlbum(user1.id(), "sample album");
         List<Card> cards = albumsService.buyCards(user1.id(), 5);
-        Card card = albumsService.addCardToAlbum(cards.getFirst().id(), album1.id());
+        albumsService.addCardToAlbum(cards.getFirst().id(), album1.id());
 
         Optional<TradingUser> userCards = albumsService.getUserWithCardsAndAlbums(user1.id());
         assertThat(userCards, notNullValue());
--- chapter7/recipe7-2/football/src/test/java/com/packt/football/service/UsersServiceTest.java
+++ chapter7/recipe7-2/football/src/test/java/com/packt/football/service/UsersServiceTest.java
@@ -12,37 +12,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.context.annotation.Import;
+
+import com.packt.football.config.FootballContainersConfig;
+
 
 import com.packt.football.domain.User;
 
 @SpringBootTest
-@Testcontainers
-@ContextConfiguration(initializers = UsersServiceTest.Initializer.class)
+@Import(FootballContainersConfig.class)
 class UsersServiceTest {
-
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
-            .withDatabaseName("football")
-            .withUsername("football")
-            .withPassword("football");
-
-    static class Initializer
-            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                            "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                            "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                            "spring.datasource.password=" + postgreSQLContainer.getPassword())
-                    .applyTo(configurableApplicationContext.getEnvironment());
-        }
-    }
-
-    @BeforeAll
-    public static void startContainer() {
-        postgreSQLContainer.start();
-    }
 
     @Autowired
     UsersService usersService;
